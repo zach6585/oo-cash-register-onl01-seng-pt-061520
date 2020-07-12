cfe3b64cdad18b534b841348f317c691491c257f
zachary mines <zmines@wesleyan.edu>
2020-07-12 21:57:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,9 @@ class CashRegister
   def add_item(title, price, quantity = nil)
     if quantity != nil 
       @total += price*quantity
-      
+      quantity.loop do 
+        @items << title 
+      @total 
     else 
       @total += price 
     end 

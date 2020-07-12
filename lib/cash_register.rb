c8591252c97cd8cddba17ef8f1aa6390069af132
zachary mines <zmines@wesleyan.edu>
2020-07-12 21:47:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,7 +25,7 @@ class CashRegister
       puts "There is no discount to apply."
       @total
     else 
-      puts "After the discount, the total comes to #{@total - @total*@discount div(100)}."
+      puts "After the discount, the total comes to #{@total - @total*@discount.div(100)}."
       @total -= @discount*@total/100 
       @total
     end 

0410bae4097d1c1a97efd724a83597e0c2330ca7
zachary mines <zmines@wesleyan.edu>
2020-07-12 21:34:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,13 @@
 class CashRegister
   
-  attr_accessor :discount
+  attr_accessor :total, :discount
   
   def initialize(discount = nil)
     @total = 0 
     @discount = discount.to_i
   end 
   
-  def total 
-    @total 
-  end 
+  
   
   def add_item(title, price)
     @total += price
